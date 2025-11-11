87247896074f36b6e4cd8a7a930e534e92ba1e72
sindhu hegde <sindhuhegde2007@gmail.com>
2025-11-11 11:57:10 +0530

    Fix indentation and add script name variable

--- si.py
+++ si.py
@@ -5,7 +5,7 @@ if len(sys.argv) != 4:
     print("Usage: python simple_interest.py <principal> <rate> <time>")
     sys.exit(1)
 
-
+script_name=float(sys.argv[0])
 principal = float(sys.argv[1])
 rate = float(sys.argv[2])
 time = float(sys.argv[3])

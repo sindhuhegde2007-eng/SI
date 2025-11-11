19966d9882f4f3c4a1ffcc76ddb4ab27de763338
sindhu hegde <sindhuhegde2007@gmail.com>
2025-11-11 11:49:35 +0530

    Refactor simple interest calculation to accept user input

--- si.py
+++ si.py
@@ -1,5 +1,17 @@
-principal=1000
-rate=200
-time=100
-simple_interest=(principal*rate*time)/100
-print("simple interest is",simple_interest)
+import sys
+
+
+if len(sys.argv) != 4:
+    print("Usage: python simple_interest.py <principal> <rate> <time>")
+    sys.exit(1)
+
+
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
+
+
+simple_interest = (principal * rate * time) / 100
+
+
+print(f"Simple Interest: {simple_interest}")
